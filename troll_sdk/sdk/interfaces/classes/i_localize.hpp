f20b12bea3854a4ad0332d46d81ff511f9cbf683
thetrollerro <43667827+thetrollerro@users.noreply.github.com>
2020-11-27 16:19:21 +0200

    fixed i_localize and added convertor to wstring to string if u need

--- troll_sdk/sdk/interfaces/classes/i_localize.hpp
+++ troll_sdk/sdk/interfaces/classes/i_localize.hpp
@@ -3,8 +3,8 @@
 
 class i_localize {
 public:
-	char* find( const char* token_name ) {
-		using original_fn = char* ( __thiscall* )( i_localize*, const char* );
-		return ( *( original_fn** ) this )[ 11 ]( this, token_name );
+	wchar_t* find( const char* token_name ) {
+		using fn = wchar_t* ( __thiscall* )( i_localize*, const char* );
+		return utils::call_virtual< fn >( this, 11 )( this, token_name );
 	}
 };
--- troll_sdk/sdk/interfaces/interfaces.hpp
+++ troll_sdk/sdk/interfaces/interfaces.hpp
@@ -111,3 +111,13 @@ constexpr int time2ticks( float t ) {
 constexpr float ticks2time( int t ) {
 	return static_cast< float >( t ) * i::globalvars->m_interval_per_tick;
 }
+
+inline std::string ws2s( const std::wstring& s )
+{
+	int len;
+	int slength = ( int ) s.length( ) + 1;
+	len = WideCharToMultiByte( CP_ACP, 0, s.c_str( ), slength, 0, 0, 0, 0 );
+	std::string r( len, '\0' );
+	WideCharToMultiByte( CP_ACP, 0, s.c_str( ), slength, &r[ 0 ], len, 0, 0 );
+	return r;
+}
